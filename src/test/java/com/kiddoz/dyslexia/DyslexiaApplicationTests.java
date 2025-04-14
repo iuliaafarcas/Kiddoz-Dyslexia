cb32380a357f37b668ec0c4d6743787691351fb0
Iulia Farcas <iuliafarcas@yahoo.com>
2025-04-14 23:02:07 +0300

    'cool'

---
+++ src/test/java/com/kiddoz/dyslexia/DyslexiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiddoz.dyslexia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DyslexiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
